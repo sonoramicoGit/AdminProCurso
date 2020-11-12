60024f74703e74af455e9cff05dab963c234aa85
rbrc <rreyprogramacion@gmail.com>
2020-11-12 13:34:28 -0600

    primera version secc3 estructuracion proyecto AdminPro

---
+++ src/app/shared/breadcrumbs/breadcrubs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrubs',
+  templateUrl: './breadcrubs.component.html',
+  styles: [
+  ]
+})
+export class BreadcrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
